6da64fd9484ec162de878adef2f8a84812822b72
Juan Cruz <ferreyra.juancruz95@gmail.com>
2026-02-21 20:04:28 -0500

    fix: allow for zooming the image

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -1,14 +1,18 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
 	var images = [], captions = [], current = 0;
 
 	function showImage(i) {
 		$('#lx-img').attr('src', images[i]);
 		$('#lx-caption').text(captions[i] || '');
+		$('#lx-img').css('cursor', 'zoom-in');
+		$('#lx-img').off('click').on('click', function () {
+			window.open(images[i], '_blank');
+		});
 	}
 
 	// Open lightbox on image click
-	$('.work-item').on('click', '.project-gallery .image', function(e) {
+	$('.work-item').on('click', '.project-gallery .image', function (e) {
 		e.preventDefault();
 
 		var $gallery = $(this).closest('.project-gallery');
@@ -16,7 +20,7 @@ $(document).ready(function() {
 
 		// Build image and caption lists from this gallery
 		images = []; captions = [];
-		$gallery.find('a.image').each(function() {
+		$gallery.find('a.image').each(function () {
 			images.push($(this).attr('href'));
 			captions.push($(this).data('caption') || '');
 		});
@@ -30,14 +34,14 @@ $(document).ready(function() {
 
 		// Tags
 		var tagsHtml = '';
-		$article.find('.lx-tag').each(function() {
+		$article.find('.lx-tag').each(function () {
 			tagsHtml += '<span>' + $(this).text() + '</span>';
 		});
 		$('#lx-tags').html(tagsHtml);
 
 		// Links
 		var linksHtml = '';
-		$article.find('.icons a').each(function() {
+		$article.find('.icons a').each(function () {
 			linksHtml += '<a href="' + $(this).attr('href') + '" title="' + $(this).attr('title') + '" class="' + $(this).attr('class') + '" target="_blank"><span class="label">' + $(this).find('.label').text() + '</span></a>';
 		});
 		$('#lx-links').html(linksHtml);
@@ -48,30 +52,30 @@ $(document).ready(function() {
 	});
 
 	// Navigation
-	$('#lx-next').on('click', function() {
+	$('#lx-next').on('click', function () {
 		current = (current + 1) % images.length;
 		showImage(current);
 	});
 
-	$('#lx-prev').on('click', function() {
+	$('#lx-prev').on('click', function () {
 		current = (current - 1 + images.length) % images.length;
 		showImage(current);
 	});
 
 	// Close: X button
-	$('#lx-close').on('click', function() {
+	$('#lx-close').on('click', function () {
 		$('#lx-overlay').removeClass('active');
 	});
 
 	// Close: click outside panel
-	$('#lx-overlay').on('click', function(e) {
+	$('#lx-overlay').on('click', function (e) {
 		if ($(e.target).is('#lx-overlay')) {
 			$('#lx-overlay').removeClass('active');
 		}
 	});
 
 	// Close: Escape key
-	$(document).on('keydown', function(e) {
+	$(document).on('keydown', function (e) {
 		if (e.key === 'Escape') $('#lx-overlay').removeClass('active');
 	});
 
